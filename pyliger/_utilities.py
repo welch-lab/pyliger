35a37a1d4dead49bf636d1ba8b849e23d095bc97
Lu Lu <luluhz@umich.edu>
2020-09-09 23:01:59 -0400

    Update _utilities.py
    function refine due to the adata structure changes

--- pyliger/_utilities.py
+++ pyliger/_utilities.py
@@ -6,64 +6,31 @@ from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 
 def merge_sparse_data_all(adata_list, library_names = None):
-    """ Function to merage all sparse data into a single one
+    """ Function to merge all sparse data into a single one
     
-    Function takes in a list of DGEs, with gene rownames and cell colnames, 
+    Function takes in a list of DGEs, with gene row names and cell column names,
     and merges them into a single DGE.
     Also adds library_names to cell_names if expected to be overlap (common with 10X barcodes)
     
-    Args:
-        adata_list(list): 
-            List of AnnData objects which store expression matrices (gene by cell).
-        library_names(list):  optional, (default None)
+    Parameters
+    ----------
+    adata_list : AnnData
+        List of AnnData objects which store expression matrices (gene by cell).
+    library_names : list, optional
+        (the default is None)
     
-    Return:
-        M():
-            AnnData object store expression matrix across datasets
+    Returns
+    -------
+    merged_adata : AnnData
+        AnnData object stores expression matrix across datasets
     """
-    """
-    col_offset = 0
-    allGenes = 
-    allCells = []
-    
-    for i in range(len(adata_list)):
-        curr = datalist[i]
-        curr_s = 
-        
-        # Now, alter the indexes so that the two 3-column matrices can be properly merged.
-        # First, make the current and full column numbers non-overlapping.
-        curr_s[] = 
-        
-        # Update full cell names
-        if library_names is not None:
-            cellnames = 
-        else:
-            cellnames = 
-            
-        allCells = 
-        
-        # Next, change the row (gene) indexes so that they index on the union of the gene sets,
-        # so that proper merging can occur.
-        idx = 
-        newgenescurr = 
-        curr_s
-        
-        # Now bind the altered 3-column matrices together, and convert into a single sparse matrix.
-        if not full_mat:
-            full_mat = curr_s
-        else:
-            full_mat = 
-        
-        col_offset = len(allCells)
-        
-    M = 
-    """
+
     merged_adata = AnnData()
     
     for adata in adata_list:
-        merged_adata = merged_adata.concatenate(adata.T, join='inner')
+        merged_adata = merged_adata.concatenate(adata, join='inner')
     
-    return merged_adata.T
+    return merged_adata
 
 def run_knn(H, k):
     """ """
@@ -169,7 +136,7 @@ def compute_snn(knn, prune):
     rows, columns = snn.nonzero()
     data = snn.data/(k+(k-snn.data))
     data[data<prune] = 0
-
+    
     return csr_matrix((data, (rows, columns)), shape=(num_cells, num_cells))
 
 def build_igraph(snn):
