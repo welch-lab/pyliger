e5d1c5dab6ded6b8460d8ae4c7c677ec1245b9c0
Andrew Robbins <andrew@robbinsa.me>
2024-06-18 19:20:52 -0400

    correct typo in plot_gene_loadings

--- src/pyliger/tools/_marker.py
+++ src/pyliger/tools/_marker.py
@@ -111,7 +111,7 @@ def get_factor_markers(
         expr_mat = vstack(expr_mat)
         cell_label = np.concatenate(
             (np.repeat(dataset1, np.sum(labels[dataset1] == factor)),
-            np.repeat(dataset1, np.sum(labels[dataset2] == factor))),
+            np.repeat(dataset2, np.sum(labels[dataset2] == factor))),
         )
         wilcoxon_result = _wilcoxon._wilcoxon(np.log(expr_mat.toarray() + 1e-10), cell_label)
 
