b7101a9c15791e3d566ebea5ff82e7521bd590f5
Andrew Robbins <andrew@robbinsa.me>
2025-03-05 17:33:46 -0500

    lazy_load typo in online_inmf

--- src/pyliger/factorization/_online_iNMF.py
+++ src/pyliger/factorization/_online_iNMF.py
@@ -1,5 +1,5 @@
 import lazy_loader as lazy
-h5sparse = np = lazy.load("h5sparse", error_on_import=True)
+h5sparse = lazy.load("h5sparse", error_on_import=True)
 np = lazy.load("numpy", error_on_import=True)
 from scipy.sparse import vstack
 from tqdm import tqdm
